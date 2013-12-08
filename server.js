6f8736bd266b18a46821fe10dcc15376852aac02
Willem Ellis <willem.ellis@gmail.com>
2013-12-08 15:51:36 -0600

    detailed error for unallowed origin requests

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var allowCrossDomain = function(req, res, next) {
          res.header('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
          next();
      } else {
-         res.send({auth: false});
+         res.send({responseText: "Invalid origin"}, 400);
      }
 }
 
