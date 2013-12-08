443078d38470cddc547ddac44d18fde04d4581bb
Willem Ellis <willem.ellis@gmail.com>
2013-12-08 15:46:30 -0600

    define allowed hosts

--- server.js
+++ server.js
@@ -10,29 +10,19 @@ var express = require('express'),
 var app = express();
 
 var allowCrossDomain = function(req, res, next) {
-    // Added other domains you want the server to give access to
-    // WARNING - Be careful with what origins you give access to
-    // var allowedHost = [
-    //     'http://backbonetutorials.com',
-    //     'http://localhost',
-    //     'http://localhost:3000'
-    // ];
+     var allowedHost = [
+         'http://localhost'
+     ];
 
-    // if(allowedHost.indexOf(req.headers.origin) !== -1) {
-    //     res.header('Access-Control-Allow-Credentials', true);
-    //     res.header('Access-Control-Allow-Origin', req.headers.origin)
-    //     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    //     res.header('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
-    //     next();
-    // } else {
-    //     res.send({auth: false});
-    // }
-
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
-    next();
+     if(allowedHost.indexOf(req.headers.origin) !== -1) {
+         res.header('Access-Control-Allow-Credentials', true);
+         res.header('Access-Control-Allow-Origin', req.headers.origin)
+         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+         res.header('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+         next();
+     } else {
+         res.send({auth: false});
+     }
 }
 
 app.configure(function(){
