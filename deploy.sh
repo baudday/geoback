4beb0ee11e9bab96546c37be0a3ead7e3910ba9f
Willem Ellis <willem.ellis@gmail.com>
2014-02-05 19:34:34 -0500

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ export COMMIT=$(git rev-parse --short HEAD)
 export FILE=$COMMIT.tar.gz
 export HOST=georeliefs.com
 
-tar -cvzf $FILE --exclude='.git/*' --exclude='tmp/*' --exclude='package.json' --exclude='README.md' --exclude='.gitignore' --exclude='env/*' --exclude='deploy.sh' *
-scp -r $FILE ubuntu@$HOST:/apps/arcapp-releases/ && sudo rm $FILE &&
-ssh ubuntu@$HOST "sudo su && cd /apps/arcapp-releases/ && mkdir $COMMIT && tar -xf $FILE -C $COMMIT && rm $FILE && cd /apps/arcapp-releases/$COMMIT && mkdir -p tmp/logs/ && cp /apps/arcapp-env/ env/"
+tar -cvzf $FILE --exclude='.git/*' --exclude='tmp/*' --exclude='README.md' --exclude='.gitignore' --exclude='env/*' --exclude='deploy.sh' *
+scp -r $FILE ubuntu@$HOST:/home/ubuntu/ && sudo rm $FILE &&
+ssh ubuntu@$HOST "cd /apps/arcapp-releases/ && sudo mkdir $COMMIT && sudo tar -xf /home/ubuntu/$FILE -C $COMMIT && rm /home/ubuntu/$FILE && cd /apps/arcapp-releases/$COMMIT && sudo mkdir -p tmp/logs/ && sudo cp /apps/arcapp-env/* env/ && cd /apps/arcapp-releases && sudo rm current && sudo ln -s /apps/arcapp-releases/$COMMIT /apps/arcapp-releases/current && sudo service arcapp restart"
