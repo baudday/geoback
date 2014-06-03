dc2b76d318d11e2be59e5105e4ca152aa81f6c2f
Willem Ellis <willem.ellis@gmail.com>
2014-06-02 21:46:00 -0400

    modify for deploying to dev server

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
 export BRANCH=$1
+export ENV=$2
 git checkout $BRANCH
 
 export COMMIT=$(git rev-parse --short HEAD)
 export FILE=$COMMIT.tar.gz
-export HOST=georeliefs.com
+
+if [ $ENV -eq "dev" ] ; then
+  export HOST="162.249.6.123"
+  export USER="root"
+  export PORT=7822
+fi
+
+if [ $ENV -eq "prod" ] ; then
+  export HOST="georeliefs.com"
+  export USER="ubuntu"
+  export PORT=22
+fi
 
 tar -cvzf $FILE --exclude='.git/*' --exclude='tmp/*' --exclude='README.md' --exclude='.gitignore' --exclude='env/*' --exclude='deploy.sh' *
-scp -r $FILE ubuntu@$HOST:/home/ubuntu/ && sudo rm $FILE &&
-ssh ubuntu@$HOST "cd /apps/arcapp-releases/ && sudo mkdir $COMMIT && sudo tar -xf /home/ubuntu/$FILE -C $COMMIT && rm /home/ubuntu/$FILE && cd /apps/arcapp-releases/$COMMIT && sudo mkdir -p tmp/logs/ && sudo cp /apps/arcapp-env/* env/ && cd /apps/arcapp-releases && sudo rm /apps/arcapp && sudo ln -s /apps/arcapp-releases/$COMMIT /apps/arcapp && sudo service arcapp restart"
+scp -r -p $PORT $FILE $USER@$HOST:/home/$USER/ && sudo rm $FILE &&
+ssh -p $PORT $USER@$HOST "cd /apps/arcapp-releases/ && sudo mkdir $COMMIT && sudo tar -xf /home/ubuntu/$FILE -C $COMMIT && rm /home/ubuntu/$FILE && cd /apps/arcapp-releases/$COMMIT && sudo mkdir -p tmp/logs/ && sudo cp /apps/arcapp-env/* env/ && cd /apps/arcapp-releases && sudo rm /apps/arcapp && sudo ln -s /apps/arcapp-releases/$COMMIT /apps/arcapp && sudo service arcapp restart"
